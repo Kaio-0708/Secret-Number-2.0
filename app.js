85f2f23168d8583c5eeebee234bfe272f3880dd1
Kaio Vitor Santos Queiroz <123708201+Kaio-0708@users.noreply.github.com>
2024-03-02 16:40:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10000;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag,texto) {
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Número Secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10000');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
